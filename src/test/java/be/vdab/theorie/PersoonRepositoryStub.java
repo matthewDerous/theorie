09bf4795c66e42480a558a3a54978261efe08f2e
matthew <matthew.derous@hotmail.com>
2025-03-05 15:25:10 +0100

    theorie deel 8

---
+++ src/test/java/be/vdab/theorie/PersoonRepositoryStub.java
@@ -0,0 +1,13 @@
+package be.vdab.theorie;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class PersoonRepositoryStub extends PersoonRepository {
+    @Override List<BigDecimal> findAllWeddes() {
+        return Stream.of(2,4,4,4,5,5,7,9)
+                .map(getal -> BigDecimal.valueOf(getal))
+                .toList();
+    }
+}
--- src/test/java/be/vdab/theorie/PersoonServiceTest.java
+++ src/test/java/be/vdab/theorie/PersoonServiceTest.java
@@ -2,13 +2,17 @@ package be.vdab.theorie;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+
 class PersoonServiceTest {
     private PersoonService service;
     @BeforeEach
     void beforeEach() {
-        service = new PersoonService(new PersoonRepository());
+        service = new PersoonService(new PersoonRepositoryStub());
+    }
+    @Test void deWeddeStandaardAfwijkingIs2() {
+        assertThat(service.standaardAfwijkingWeddes()).isEqualByComparingTo("2");
     }
     @Test
     void deWeddeStandaardAfwijkingIsPositief() {
